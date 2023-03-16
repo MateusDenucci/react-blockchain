99aa9430e779cb96840c4e9197799b83fbaa0fb3
mresende <mresende@pinterest.com>
2023-03-16 16:49:57 -0700

    Basic funtionality to add a block to the blockchain wo PoW

--- src/components/MineBlock.js
+++ src/components/MineBlock.js
@@ -1,9 +1,26 @@
-function MineBlock() {
+import { useState } from "react";
+
+function MineBlock({ handleMining }) {
+  const [blockPayload, setBlockPayload] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleMining(blockPayload);
+    setBlockPayload("");
+  };
+
   return (
     <div>
-      <textarea rows={5}></textarea>
-      <hr></hr>
-      <button>Mine now</button>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          rows={5}
+          maxLength={200}
+          onChange={(e) => setBlockPayload(e.target.value)}
+          value={blockPayload}
+        ></textarea>
+        <hr></hr>
+        <button>Mine now</button>
+      </form>
     </div>
   );
 }
--- src/pages/MiningPage.js
+++ src/pages/MiningPage.js
@@ -3,10 +3,20 @@ import DisplayBlockchain from "../components/DisplayBlockchain";
 import MineBlock from "../components/MineBlock";
 
 function MiningPage() {
+  const [blockchain, setBlockchain] = useState([]);
+  const [isMining, setIsMining] = useState(false);
+
+  const handleMining = (payload) => {
+    const block = {
+      payload,
+    };
+    setBlockchain([...blockchain, block]);
+  };
+
   return (
     <div>
       <h1>Blockchain toy</h1>
-      <MineBlock />
+      <MineBlock handleMining={handleMining} />
       <hr></hr>
       <DisplayBlockchain />
     </div>
