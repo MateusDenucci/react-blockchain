98d355caf5113d082be9c3110ff34e6269a7282e
mresende <mresende@pinterest.com>
2023-03-16 17:58:06 -0700

    Displaying blockchain

--- src/components/DisplayBlockchain.js
+++ src/components/DisplayBlockchain.js
@@ -1,6 +1,21 @@
 function DisplayBlockchain({ blockchain }) {
-  console.log(blockchain);
-  return <div>Blockchain here</div>;
+  const renderedBlockchain = blockchain.map((block) => {
+    return (
+      <div
+        key={block.hash}
+        style={{
+          borderStyle: "solid",
+          width: "800px",
+          margin: "10px 0px 0px 10px",
+        }}
+      >
+        <p>paylod: {block.payload}</p>
+        <p>nonce: {block.nonce}</p>
+        <p>hash: {block.hash}</p>
+      </div>
+    );
+  });
+  return <div>Blockchain here: {renderedBlockchain}</div>;
 }
 
 export default DisplayBlockchain;
